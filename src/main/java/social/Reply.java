989f6ae9ad179e360de708b176806a03ea6e9426
Daryl Ang <darylang.2017@sis.smu.edu.sg>
2020-02-25 21:03:01 +0800

    MAVEN

--- src/main/java/social/Reply.java
+++ src/main/java/social/Reply.java
@@ -2,14 +2,14 @@ package social;
 
 public class Reply{
     private String message;
-    private Member poster;
+    // private Member poster;
 
-    public Reply(String message, Member poster){
-        this.message = message;
-        this.poster = poster; 
-    }
+    // public Reply(String message, Member poster){
+    //     this.message = message;
+    //     this.poster = poster; 
+    // }
 
-    public String getMessage(){
-        return message;
-    }
+    // public String getMessage(){
+    //     return message;
+    // }
 }
