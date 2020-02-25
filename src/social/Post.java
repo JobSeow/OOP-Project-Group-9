3d1b6ab1d40bd0e423bb4d92b9475269e5d00a30
Daryl Ang <darylang.2017@sis.smu.edu.sg>
2020-02-25 17:24:46 +0800

    started on Post

---
+++ src/social/Post.java
@@ -0,0 +1,67 @@
+// package social;
+
+// import social.Reply;
+import java.util.*;
+
+
+public class Post{
+    private int likes;
+    private int dislikes;
+    private String message;
+    private List<String> taggedUsers = new ArrayList<String>();
+    // private List<Reply> replies = new ArrayList<Reply>();
+    private Date datePosted;
+
+    // constructor
+    public Post(String message){
+        String[] splitMessages = message.split(" ");
+        for (int i = 0; i < splitMessages.length; i++){
+            String part = splitMessages[i];
+            if (part.charAt(0) == '@'){
+                String usernameChunk = part.substring(1);
+                String username = usernameChunk.split("[^\\w]+")[0];
+                // need to check if username exists here
+                taggedUsers.add(username);
+                splitMessages[i] = usernameChunk;
+            }
+        }
+        String finalMessage = String.join(" ", splitMessages);
+        this.message = finalMessage;
+        this.datePosted = new Date();
+        // broadcast this message to all taggedusers.
+        // broadcastMessage();
+    }
+    
+    // getters
+    public int getLikes(){
+        return likes;
+    }
+    public int getDislikes(){
+        return dislikes;
+    }
+    public String getMessage(){
+        return message;
+    }
+    public Date getDate(){
+        return datePosted;
+    }
+    public List getTaggedUsers(){
+        return taggedUsers;
+    }
+
+    // public void addReply(Reply reply){
+    //     replies.add(reply);
+    // }
+    // public void broadcastMessage(){
+    //     for (String user : taggedUsers){
+            
+    //     }
+    // }
+
+    public static void main(String[] args) {
+        String msg = "Until now, @evelyn, @adeline, @marilyn's laptops have bitten the dust. See, OOP is tough on laptops as well :/";
+        Post p = new Post(msg);
+        System.out.println(p.getMessage());
+        System.out.println(p.getDate());
+    }
+}
---
+++ src/social/Reply.java
@@ -0,0 +1,15 @@
+package social;
+
+public class Reply{
+    private String message;
+    private Member poster;
+
+    public Reply(String message, Member poster){
+        this.message = message;
+        this.poster = poster; 
+    }
+
+    public String getMessage(){
+        return message;
+    }
+}
