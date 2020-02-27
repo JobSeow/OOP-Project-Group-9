e3db0ab679ccbc940a108cd9045ecb6bf640d6f6
Daryl Ang <darylang.2017@sis.smu.edu.sg>
2020-02-27 15:29:45 +0800

    start on member

--- src/main/java/social/Member.java
+++ src/main/java/social/Member.java
@@ -1,3 +1,31 @@
+package social;
+
+import java.util.*;
+
 public class Member{
-    
+    private String fullName;
+    private String rank;
+    private String username;
+    private List<Post> postList;
+
+    public Member(String fullName, String username){
+        this.fullName = fullName;
+        this.username = username;
+    }
+
+    public String getFullName(){
+        return fullName;
+    }
+
+    public String getRank(){
+        return rank;
+    }
+
+    public String getUserName(){
+        return username;
+    }
+
+    public List<Post> getPostList(){
+        return postList;
+    }
 }
